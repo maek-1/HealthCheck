a969a62163e8a3813674d91cce6eb10b73b9f6a3
maek-1 <maximyanuk230408@gmail.com>
2025-07-17 19:25:24 +0300

    Add project files

---
+++ my_app.py
@@ -0,0 +1,35 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget,QLabel,QPushButton, QButtonGroup, QRadioButton, QHBoxLayout, QMessageBox, QVBoxLayout
+from instr import *
+from second_win import *
+
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.set_appear()
+        self.show()
+        self.connects()
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.btn_next = QPushButton(txt_next, self)
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)
+        self.setLayout(self.layout_line)
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
+
+
+app = QApplication([])
+main_window = MainWin()
+app.exec_()     
